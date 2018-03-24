e0e9be818297e0766800206a5b6a29d279623c5f
Sheerin Naseem <sheerin.naseem@gmail.com>
2018-03-25 02:37:36 +0530

    Scene 4 with score system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
 
+    public Text countText;
+
+    public Text winText;
+
     private Rigidbody2D rb2d;
+
+    private int count;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D> () ;
+        count = 0;
+        winText.text = "";
+        SetCountText();
     }
 
     void FixedUpdate()
@@ -26,7 +36,18 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("Coin"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
         }
         
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 2)
+        {
+            winText.text = "You win";
+        }
     }
 }
