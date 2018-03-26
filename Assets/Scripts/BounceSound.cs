082d2c061734f32785e62c13e00843c49d021b0e
Sheerin Naseem <sheerin.naseem@gmail.com>
2018-03-26 17:23:11 +0530

    Phonics sound included

--- Assets/Scripts/BounceSound.cs
+++ Assets/Scripts/BounceSound.cs
@@ -11,14 +11,5 @@ public class BounceSound : MonoBehaviour {
         audioSource = GetComponent<AudioSource>();
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        foreach (ContactPoint contact in collision.contacts)
-        {
-            Debug.DrawRay(contact.point, contact.normal, Color.white);
-        }
-
-        if (collision.relativeVelocity.magnitude > 2)
-            audioSource.Play();
-    }
+    
 }
